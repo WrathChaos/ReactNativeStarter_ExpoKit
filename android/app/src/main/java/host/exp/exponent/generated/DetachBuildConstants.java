0fa1e02a1969f75c3647ac0db96a8c04f4af8ad5
FreakyCoder <kurayogun@Kurays-MacBook-Pro.local>
2018-10-17 13:27:35 +0300

    PackageJSON is updated and new image is added

--- android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
+++ android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
@@ -3,6 +3,6 @@ package host.exp.exponent.generated;
 // This file is auto-generated. Please don't rename!
 public class DetachBuildConstants {
 
-  public static final String DEVELOPMENT_URL = "";
+  public static final String DEVELOPMENT_URL = "exp544815e58df34311aac6d013e17d4f39://192.168.1.10:19000";
 
 }
